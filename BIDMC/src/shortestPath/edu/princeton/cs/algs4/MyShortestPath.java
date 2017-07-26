ef8385a7bebd21a158cc8ce0146494df04d9661a
wavefancy <wavefancy@gmail.com>
2017-07-26 14:33:46 -0400

    version1.2

--- BIDMC/src/shortestPath/edu/princeton/cs/algs4/MyShortestPath.java
+++ BIDMC/src/shortestPath/edu/princeton/cs/algs4/MyShortestPath.java
@@ -6,9 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
@@ -18,7 +21,7 @@ import org.docopt.Docopt;
  * 
  * @author wavefancy@gmail.com
  * 
- * @version 1.0
+ * @version 1.2
  * Compute shortest path based on princeton's code base.
  * http://algs4.cs.princeton.edu/44sp/
  * all code on github: https://github.com/kevin-wayne/algs4
@@ -33,12 +36,13 @@ public class MyShortestPath {
     private static int RandomPickGenes = -1; //random pick this number of genes.
     private final static Map<String,Integer> geneNameMap = new HashMap(); //map genename -> int number.
     private static int  tempIndex = 0;
+    private static int outputClosestNKnownGenes = -1; //output closest N knownGene for each topGenes.
 	
     private static final String DOC =
                 "MyUndirectedWeightedShortestPath.\n"
                 + "\n"
                 + "Usage:\n"
-                + "  MyUndirectedWeightedShortestPath -k knownGene (-i topGenes | -r int -b int) [-t cpus] \n"
+                + "  MyUndirectedWeightedShortestPath -k knownGene (-i topGenes [-c int] | -r int -b int) [-t cpus] \n"
                 + "  MyUndirectedWeightedShortestPath (-h | --help)\n"
                 + "  MyUndirectedWeightedShortestPath --version\n"
                 + "\n"
@@ -53,6 +57,7 @@ public class MyShortestPath {
                 + "Options:\n"
                 + "  -k knownGene  Input known gene list, one line one gene.\n"
                 + "  -i topGenes   Input top gene list, one line one gene.\n"
+                + "  -c int        Output the closest [int] knownGenes for each topGenes.\n"
                 + "  -r int        Number of random picked genes for bootstrapping.\n"
                 + "  -b int        Number of bootstrappings.\n"
                 + "  -t cpus       Number of cpus for computing.\n"
@@ -90,10 +95,59 @@ public class MyShortestPath {
                         .filter(s-> s>=0 )
                         .average();
     }
+    
+    /**
+     * Compute sorted distance to targetGenes and corresponding targetGene ID.
+     * @param testGeneID
+     * @param targetGeneIDs
+     * @param G
+     * @return 
+     */
+    private static List sortedDistanceAndID(int testGeneID, int [] targetGeneIDs, EdgeWeightedGraph G){
+        DijkstraUndirectedSP sp = new DijkstraUndirectedSP(G, testGeneID);
+        
+        //get the distance to all targetGenes.
+        double[] distance = new double[targetGeneIDs.length];
+        for (int i = 0; i < targetGeneIDs.length; i++) {
+            int targetGeneID = targetGeneIDs[i];
+            if (sp.hasPathTo(targetGeneID)) {
+                distance[i] = sp.distTo(targetGeneID);
+            }else{
+                distance[i] = Double.MAX_VALUE;
+            }
+        }
+        
+        //find the closest one.
+        double[][] results = new double[distance.length][2]; //array of (distance, targetGeneID)
+        for (int i = 0; i < results.length; i++) {
+            double[] r = new  double[2];
+            r[0] = distance[i];
+            r[1] = targetGeneIDs[i];
+            results[i] = r;
+        }
+        
+        List<double[]> reList = Arrays.asList(results);
+        Collections.sort(reList,new Comparator<double[]>(){
+                    public int compare(double[] i, double[] j){
+                        double t =  i[0] - j[0];
+                        if(t==0){
+                            return 0;
+                        }else if (t>0) {
+                            return 1;
+                        }else{
+                            return -1;
+                        }
+                    }
+                });
+//        for (double[] ds : reList) {
+//            System.out.println(Arrays.toString(ds));
+//        }
+        return reList;
+    }
 
     public static void main(String[] args) {
             Map<String, Object> opts =
-                 new Docopt(DOC).withVersion("1.1").parse(args);
+                 new Docopt(DOC).withVersion("1.2").parse(args);
 //		     System.err.println(opts);
             if(opts.get("-t") != null){
                     System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", (String) opts.get("-t"));
@@ -119,6 +173,13 @@ public class MyShortestPath {
                    
                 } catch (Exception e) {
                     e.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+            if(opts.get("-c") != null){
+                outputClosestNKnownGenes = Integer.parseInt((String) opts.get("-c"));
+                if(topGenes == null){
+                    System.err.println("ERROR: -c option must be used in combination with -i option!");
                     System.exit(-1);
                 }
             }
@@ -197,12 +258,51 @@ public class MyShortestPath {
                         .toArray();
                 //System.out.println(Arrays.toString(knwonGeneIDs));
 //                System.out.println(Arrays.toString(topGeneIDs));
-                OptionalDouble results = averageMindistance(topGeneIDs, knwonGeneIDs, G);
-                //System.out.println(decimalFormat.format(results));
-                if (results.isPresent()) {
-                    System.out.println(decimalFormat.format(results.getAsDouble()));
-                }else{
-                    System.out.println("NA");
+//                System.out.println(outputClosestNKnownGenes);
+                if (outputClosestNKnownGenes <= 0) {
+                     OptionalDouble results = averageMindistance(topGeneIDs, knwonGeneIDs, G);
+                    //System.out.println(decimalFormat.format(results));
+                    if (results.isPresent()) {
+                        System.out.println(decimalFormat.format(results.getAsDouble()));
+                    }else{
+                        System.out.println("NA");
+                    }
+
+                   
+                }else{ //output the closest top n known genes for each top genes.
+//                    System.out.println(outputClosestNKnownGenes);
+                    final HashMap<Integer,String> reverseIDNameMap = new HashMap<>(geneNameMap.size());
+                    for (Map.Entry<String, Integer> entry : geneNameMap.entrySet()) {
+                        String key = entry.getKey();
+                        Integer value = entry.getValue();
+                        reverseIDNameMap.put(value, key);
+                    }
+                    
+//                    System.out.println(outputClosestNKnownGenes);
+//                    System.out.println("shortestPath.edu.princeton.cs.algs4.MyShortestPath.main()");
+                    Arrays.stream(topGeneIDs)
+                            .parallel()
+                            .forEach(s->{
+                                List<double[]> re = sortedDistanceAndID(s, knwonGeneIDs, G);
+                                StringBuilder sb = new StringBuilder();
+                                DecimalFormat formater = new  DecimalFormat("#.####");
+//                                System.out.println("shortestPath.edu.princeton.cs.algs4.MyShortestPath.main()");
+                                int len = Math.min(outputClosestNKnownGenes, re.size());
+                                for (int i = 0; i < len; i++) {
+                                    sb.setLength(0);
+                                    sb.append(i+1).append("\t");
+                                    sb.append(reverseIDNameMap.get(s)).append("\t");
+                                    double[] r = re.get(i);
+                                    if(r[0] == Double.MAX_VALUE){
+                                        sb.append("NA").append("\t");
+                                        sb.append("NA");
+                                    }else{
+                                        sb.append(reverseIDNameMap.get((int)r[1])).append("\t");
+                                        sb.append(formater.format(r[0]));
+                                    }
+                                    System.out.println(sb.toString());
+                                }
+                            });
                 }
                 
                 System.exit(0);
